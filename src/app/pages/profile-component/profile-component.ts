d09f06d8388d8e2ebfda9a788ecf031cc2050ca5
HP5000SERIES\avila <avilameza@gmail.com>
2025-12-27 17:37:55 -0500

    creacion del modulo de seleccion del perfil

---
+++ src/app/pages/profile-component/profile-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-component',
+  imports: [RouterLink, RouterModule],
+  templateUrl: './profile-component.html'
+})
+export class ProfileComponent {
+
+}
