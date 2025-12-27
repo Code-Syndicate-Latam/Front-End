a8ec8a83ad1e46f0af51df0bd401929fdda0608a
HP5000SERIES\avila <avilameza@gmail.com>
2025-12-27 17:35:16 -0500

    creacion del formulario de registro de propietario

---
+++ src/app/pages/register/register-owner-component/register-owner-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-owner-component',
+  imports: [],
+  templateUrl: './register-owner-component.html',
+})
+export class RegisterOwnerComponent {
+
+}
